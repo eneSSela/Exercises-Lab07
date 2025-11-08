53cb26d7b5be9b1cc81e883a83a8ad8741192c69
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-08 19:18:45 +0100

    Implement Transformers

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,18 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        
+        return flattenTransform(base, new Function<I,Collection<? extends O>>() {
+
+            @Override
+            public Collection<? extends O> call(I input) {
+                
+                final List<O> list = new ArrayList<>();
+                list.add(transformer.call(input));
+
+                return list;
+            }
+        });
     }
 
     /**
@@ -70,7 +81,18 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        
+        return flattenTransform(base, new Function<Collection<? extends I>,Collection<? extends I>>() {
+
+            @Override
+            public Collection<? extends I> call(Collection<? extends I> input) {
+                
+                List<I> result = new ArrayList<>();
+                result.addAll(input);
+
+                return result;
+            } 
+        });
     }
 
     /**
@@ -87,7 +109,21 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        
+        return flattenTransform(base, new Function<I,Collection<? extends I>>() {
+
+            @Override
+            public Collection<? extends I> call(I input) {
+
+                List<I> result = new ArrayList<>();
+
+                if(test.call(input)) {
+                    result.add(input);
+                }
+
+                return result;
+            } 
+        });
     }
 
     /**
@@ -103,6 +139,13 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        
+        return select(base, new Function<I,Boolean>() {
+
+            @Override
+            public Boolean call(I input) {
+                return !test.call(input);
+            } 
+        });
     }
 }
